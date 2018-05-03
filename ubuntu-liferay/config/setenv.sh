cf0fe2130d8fd3d7cee167a19c18588a2572b852
Jan Brychta <jlbenglish@gmail.com>
2018-05-03 22:47:48 +0200

    docker infrastructure

--- ubuntu-liferay/config/setenv.sh
+++ ubuntu-liferay/config/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=Europe/Rome -Xmx1024m -XX:MaxPermSize=512m -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
+CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=512m -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
