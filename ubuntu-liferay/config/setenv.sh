7ce3d23c066a28215adb62e219005fffc2b1b8d9
Jan Brychta <jlbenglish@gmail.com>
2018-05-16 14:09:01 +0200

    development mode

--- ubuntu-liferay/config/setenv.sh
+++ ubuntu-liferay/config/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=512m -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
+CATALINA_OPTS="$CATALINA_OPTS -Duser.timezone=GMT -Xmx2048m -XX:MaxPermSize=1024m -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
